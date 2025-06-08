0d30725424519502ae97f9f0c2b610c3ade2ef25
fuli.github.io <fufufufuli1005@gmail.com>
2025-06-09 06:48:30 +0800

    rename

--- program/py/main.py
+++ program/py/main.py
@@ -1,51 +1,48 @@
 import pyautogui, keyboard, time
-
-array = []
+#点击坐标数组
+click_array = []
 properties = []
 is_running = False
 with open('array.txt', 'r') as file:
     content = file.read()
     for item in content.split('\n'):
-        array.append(item.split(','))
+        click_array.append(item.split(','))
 
 with open('hotKey.txt', 'r') as file:
     content = file.read()
     for item in content.split('\n'):
         properties.append(item)
 
-start = properties[0]
-stop = properties[1]
-position = properties[2]
-calc = float(properties[3])
+key_start = properties[0]
+key_stop = properties[1]
+key_position = properties[2]
+sleep_time = float(properties[3])
 
-
-def start_logic():
+def logic_start():
     global is_running
     is_running = True
     print('已开启')
 
-
-def stop_logic():
+def logic_stop():
     global is_running
     is_running = False
     print('已暂停')
 
-
-def position_logic():
+def logic_position():
     print(pyautogui.position())
 
+# 按键事件监听
+keyboard.add_hotkey(key_start, logic_start)
+keyboard.add_hotkey(key_stop, logic_stop)
+keyboard.add_hotkey(key_position, logic_position)
 
-# 按键事件处理
-
-keyboard.add_hotkey(start, start_logic)
-keyboard.add_hotkey(stop, stop_logic)
-keyboard.add_hotkey(position, position_logic)
+#任务执行
 print('代码启动')
-print('按下' + start + '开启运行')
-print('按下' + stop + '暂停运行')
-print('按下' + position + '获取鼠标坐标')
+print('按下' + key_start + '开启运行')
+print('按下' + key_stop + '暂停运行')
+print('按下' + key_position + '获取鼠标坐标')
 while True:
     if is_running:
-        for num in array:
+        for num in click_array:
             pyautogui.click(num)
-            time.sleep(calc)
+            time.sleep(sleep_time)
